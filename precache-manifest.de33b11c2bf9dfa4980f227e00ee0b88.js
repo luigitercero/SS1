f22f50cab74f8c68cfb500ed18328c478a4a6809
luigitercero <luigitercero3@gmail.com>
2018-11-11 20:50:08 -0600

    first commit

---
+++ precache-manifest.de33b11c2bf9dfa4980f227e00ee0b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e02fad377b0376263590",
+    "url": "/static/css/main.acf96ba4.chunk.css"
+  },
+  {
+    "revision": "e02fad377b0376263590",
+    "url": "/static/js/main.e02fad37.chunk.js"
+  },
+  {
+    "revision": "341433079fbb1dcdcb92",
+    "url": "/static/css/1.049744ce.chunk.css"
+  },
+  {
+    "revision": "341433079fbb1dcdcb92",
+    "url": "/static/js/1.34143307.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af72a6d8a727846f4e3e8c5a14a184f9",
+    "url": "/index.html"
+  }
+];
